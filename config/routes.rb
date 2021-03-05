173f8e8effd1f76b13fda57baaef4f95d9146f86
Benjamin Hernandez <benjamin@coderia.mx>
2021-03-05 01:24:58 -0600

    Added automated system tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index' # And remove the generated route `products/index`
+
+  resources :products, only: [:new, :create]
 end
